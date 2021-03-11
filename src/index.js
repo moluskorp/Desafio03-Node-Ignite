1e91c87e4654f2226763c401480f1f13caa6328e
Junior Baratella <molusko.rp@hotmail.com>
2021-03-11 02:29:07 -0300

    DesafioFinalizado

---
+++ src/index.js
@@ -0,0 +1,93 @@
+const express = require("express");
+
+const { v4: uuid, validate } = require("uuid");
+
+const app = express();
+
+app.use(express.json());
+
+const repositories = [];
+
+app.get("/repositories", (request, response) => {
+  return response.json(repositories);
+});
+
+app.post("/repositories", (request, response) => {
+  const { title, url, techs } = request.body
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0
+  };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
+});
+
+app.put("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  if (id === 'undefined') {
+    return response.status(404).json({ error: 'Invalid Id' });
+  }
+
+  const repository = repositories.find(repository => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  const { title, url, techs } = request.body;
+
+  repository.title = title;
+  repository.url = url;
+  repository.techs = techs;
+
+  return response.json(repository);
+});
+
+app.delete("/repositories/:id", (request, response) => {
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
+});
+
+app.post("/repositories/:id/like", (request, response) => {
+  const { id } = request.params;
+
+  const repository = repositories.find((repository) => repository.id === id);
+
+  if (!repository) {
+    return response.status(404).json({ error: "Repository not found" });
+  }
+
+  repository.likes++;
+
+  return response.json(repository);
+
+  /*repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  const repository = repositories[repositoryIndex];*/
+
+  return response.json(repository);
+});
+
+module.exports = app;
